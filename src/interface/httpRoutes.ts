bd0283f634c3205521aeebdf4bbec814131ba988
jacobg1 <greenwald.j8@gmail.com>
2023-01-08 22:08:11 -0500

    Type request data

--- src/interface/httpRoutes.ts
+++ src/interface/httpRoutes.ts
@@ -1,4 +1,14 @@
+import { QsParams, RequestBody } from "./documents";
+
 export enum HttpRoutes {
   UPLOAD_DOC = "PUT /file/put",
   GET_DOC_BY_NAME = "GET /file/get",
 }
+
+export interface RequestData {
+  body?: RequestBody;
+  queryStringParameters?: QsParams;
+  pathParameters?: Record<string, string>;
+}
+
+export type RouteHandler<T> = (data: RequestData) => Promise<T>;
