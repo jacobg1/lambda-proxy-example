a69b12d7ff0135f996aa3d4409b5ca2a8b27af05
jacobg1 <greenwald.j8@gmail.com>
2023-01-08 22:08:00 -0500

    Refactor handler

--- src/handler.ts
+++ src/handler.ts
@@ -1,14 +1,10 @@
 import { APIGatewayProxyEventV2, APIGatewayProxyHandlerV2 } from "aws-lambda";
 import { handleUpload } from "./handlers/handleUpload";
 import { handleGetObjectUrl } from "./handlers/handleGetObjectUrl";
-import { HttpRoutes } from "./interface/httpRoutes";
-import { UploadDocRequest } from "./interface/uploadDoc";
+import { HttpRoutes, RequestData } from "./interface/httpRoutes";
 
 // TODO: schema or DTO validation
-const parseReqBody = <T>(body?: string): T => {
-  if (!body) {
-    throw new Error("Failed to parse req body, undefined body");
-  }
+const parseReqBody = (body: string): unknown => {
   try {
     return JSON.parse(body);
   } catch (e) {
@@ -16,28 +12,55 @@ const parseReqBody = <T>(body?: string): T => {
   }
 };
 
-const handleResponse = async (
-  event: APIGatewayProxyEventV2
-): Promise<unknown> => {
+const extractReqData = (event: APIGatewayProxyEventV2): RequestData | {} => {
+  const { body, queryStringParameters, pathParameters } = event;
+
+  return {
+    ...(body && { body: parseReqBody(body) }),
+    ...(queryStringParameters && { queryStringParameters }),
+    ...(pathParameters && { pathParameters }),
+  };
+};
+
+const routeConfig = {
+  [HttpRoutes.GET_DOC_BY_NAME]: handleGetObjectUrl,
+  [HttpRoutes.UPLOAD_DOC]: handleUpload,
+};
+
+const handleEvent = async (event: APIGatewayProxyEventV2) => {
   const { method, path } = event?.requestContext?.http;
   if (!method || !path) {
     throw new Error("Missing http requestContext");
   }
-  switch (`${method} ${path}`) {
-    case HttpRoutes.GET_DOC_BY_NAME:
-      const fileName = event.queryStringParameters?.fileName;
-      return handleGetObjectUrl(fileName);
-    case HttpRoutes.UPLOAD_DOC:
-      return handleUpload(parseReqBody<UploadDocRequest>(event.body));
-    default:
-      throw new Error("Unrecognized route");
+
+  const handler = routeConfig[`${method} ${path}` as HttpRoutes];
+  if (!handler) {
+    throw new Error("Missing handler");
   }
+
+  return handler(extractReqData(event));
 };
 
 export const handle: APIGatewayProxyHandlerV2 = async (event) => {
-  const response = await handleResponse(event);
-  return {
-    statusCode: 200,
-    body: typeof response === "string" ? response : JSON.stringify(response),
-  };
+  try {
+    const response = await handleEvent(event);
+    return {
+      statusCode: 200,
+      isBase64Encoded: false,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(response),
+    };
+  } catch (e) {
+    return {
+      statusCode: 500,
+      isBase64Encoded: false,
+      headers: {
+        "Content-Type": "text/plain",
+        "x-amzn-ErrorType": e.name || "Error",
+      },
+      body: e.message || "Internal Server Error",
+    };
+  }
 };
