d3d30557b87eead184041af7680da7c48a2fdcc6
jacobg1 <greenwald.j8@gmail.com>
2023-01-08 22:26:44 -0500

    Using new methods

--- src/handler.ts
+++ src/handler.ts
@@ -1,26 +1,8 @@
 import { APIGatewayProxyEventV2, APIGatewayProxyHandlerV2 } from "aws-lambda";
 import { handleUpload } from "./handlers/handleUpload";
 import { handleGetObjectUrl } from "./handlers/handleGetObjectUrl";
-import { HttpRoutes, RequestData } from "./interface/httpRoutes";
-
-// TODO: schema or DTO validation
-const parseReqBody = (body: string): unknown => {
-  try {
-    return JSON.parse(body);
-  } catch (e) {
-    throw new Error(e);
-  }
-};
-
-const extractReqData = (event: APIGatewayProxyEventV2): RequestData | {} => {
-  const { body, queryStringParameters, pathParameters } = event;
-
-  return {
-    ...(body && { body: parseReqBody(body) }),
-    ...(queryStringParameters && { queryStringParameters }),
-    ...(pathParameters && { pathParameters }),
-  };
-};
+import { HttpRoutes } from "./interface/httpRoutes";
+import { extractReqData, handleError, handleResponse } from "./utils/format";
 
 const routeConfig = {
   [HttpRoutes.GET_DOC_BY_NAME]: handleGetObjectUrl,
@@ -29,11 +11,13 @@ const routeConfig = {
 
 const handleEvent = async (event: APIGatewayProxyEventV2) => {
   const { method, path } = event?.requestContext?.http;
+
   if (!method || !path) {
     throw new Error("Missing http requestContext");
   }
 
   const handler = routeConfig[`${method} ${path}` as HttpRoutes];
+
   if (!handler) {
     throw new Error("Missing handler");
   }
@@ -44,23 +28,8 @@ const handleEvent = async (event: APIGatewayProxyEventV2) => {
 export const handle: APIGatewayProxyHandlerV2 = async (event) => {
   try {
     const response = await handleEvent(event);
-    return {
-      statusCode: 200,
-      isBase64Encoded: false,
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(response),
-    };
+    return handleResponse(response);
   } catch (e) {
-    return {
-      statusCode: 500,
-      isBase64Encoded: false,
-      headers: {
-        "Content-Type": "text/plain",
-        "x-amzn-ErrorType": e.name || "Error",
-      },
-      body: e.message || "Internal Server Error",
-    };
+    return handleError(e);
   }
 };
