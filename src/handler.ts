41c531d6d8861fe3f7336bc61bce0e75bb00878e
jacobg1 <greenwald.j8@gmail.com>
2023-01-08 22:37:11 -0500

    Update imports

--- src/handler.ts
+++ src/handler.ts
@@ -1,27 +1,23 @@
 import { APIGatewayProxyEventV2, APIGatewayProxyHandlerV2 } from "aws-lambda";
-import { handleUpload } from "./handlers/handleUpload";
+import { handleGetUploadUrl } from "./handlers/handleGetUploadUrl";
 import { handleGetObjectUrl } from "./handlers/handleGetObjectUrl";
 import { HttpRoutes } from "./interface/httpRoutes";
 import { extractReqData, handleError, handleResponse } from "./utils/format";
 
 const routeConfig = {
   [HttpRoutes.GET_DOC_BY_NAME]: handleGetObjectUrl,
-  [HttpRoutes.UPLOAD_DOC]: handleUpload,
+  [HttpRoutes.UPLOAD_DOC]: handleGetUploadUrl,
 };
 
 const handleEvent = async (event: APIGatewayProxyEventV2) => {
   const { method, path } = event?.requestContext?.http;
-
   if (!method || !path) {
     throw new Error("Missing http requestContext");
   }
-
   const handler = routeConfig[`${method} ${path}` as HttpRoutes];
-
   if (!handler) {
     throw new Error("Missing handler");
   }
-
   return handler(extractReqData(event));
 };
 
--- src/handlers/handleGetObjectUrl.ts
+++ src/handlers/handleGetObjectUrl.ts
@@ -1,10 +1,10 @@
 import { GetObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { s3Client } from "../clients/s3-client";
-import { GetDocResponse } from "../interface/documents";
+import { GetObjectUrlResponse } from "../interface/documents";
 import type { RouteHandler } from "../interface/httpRoutes";
 
-export const handleGetObjectUrl: RouteHandler<GetDocResponse> = async (
+export const handleGetObjectUrl: RouteHandler<GetObjectUrlResponse> = async (
   data
 ) => {
   const fileName = data.queryStringParameters?.fileName;
